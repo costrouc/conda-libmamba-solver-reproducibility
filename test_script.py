9ee552a44be18e1744cd896c2d2247aaac15a80a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-01-19 22:29:45 -0500

    Run tests offline

--- test_script.py
+++ test_script.py
@@ -4,6 +4,7 @@ import subprocess
 cmd = 'conda create --experimental-solver libmamba -n testtesttest -c conda-forge --override-channels python=3.8 bokeh hvplot --dry-run'
 pattern = r'\/(.+)::bokeh-(\d+.\d+.\d+)'
 
+result = subprocess.run(cmd.split(' '), capture_output=True, text=True)
 for _ in range(10):
-    result = subprocess.run(cmd.split(' '), capture_output=True, text=True)
+    result = subprocess.run(cmd.split(' ') + ["--offline"], capture_output=True, text=True)
     print(re.findall(pattern, result.stdout))
